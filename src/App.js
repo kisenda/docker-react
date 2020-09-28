dcb49559e5925dbd7ba8e144fbb024981482bab4
kisenda <kisenda@gmail.com>
2020-09-28 13:28:34 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi! my name is Kisenda, 
+          Holla baby! eka suny ma kuchi. Alhamdulillah.. :)
         </p>
         <a
           className="App-link"
